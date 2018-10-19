0c1ec8fd6e9e716bb33c08e207b276b83b399fe3
Md Habibur Rahman <habib.usa2014@gmail.com>
2018-10-19 22:10:35 +0600

    Code formet corrected and Autometic location finder added

--- app/src/main/java/com/placeremainder/place/placealertassistent/activities/MapsActivity.java
+++ app/src/main/java/com/placeremainder/place/placealertassistent/activities/MapsActivity.java
@@ -8,42 +8,75 @@ import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.AdapterView;
+import android.widget.AutoCompleteTextView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.AutocompletePrediction;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.PlaceBuffer;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.placeremainder.place.placealertassistent.adapters.PlaceAutocompleteAdapter;
+import com.placeremainder.place.placealertassistent.models.LocationInfo;
 import com.placeremainder.place.placealertassistent.services.GoogleService;
 import com.placeremainder.place.placealertassistent.R;
-
 import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+    private static final String TAG = "MapActivity";
+    private static final float DEFAULT_ZOOM = 15f;
+    private static final LatLngBounds LAT_LNG_BOUNDS = new LatLngBounds(
+            new LatLng(-40, -168), new LatLng(71, 136));
     private GoogleMap mMap;
     private static final int REQUEST_PERMISSIONS = 100;
     boolean boolean_permission;
+    @BindView(R.id.current_address)
     TextView currentAddress;
+    @BindView(R.id.remainder_address)
+    AutoCompleteTextView remainderAddress;
+    GoogleApiClient mGoogleApiClient;
+    private PlaceAutocompleteAdapter mAutocompleteAdapter;
     SharedPreferences mPref;
     SharedPreferences.Editor medit;
     Double latitude, longitude;
     Geocoder geocoder;
+    private LocationInfo mLocationInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        currentAddress = (TextView) findViewById(R.id.current_address);
+        ButterKnife.bind(this);
         geocoder = new Geocoder(this, Locale.getDefault());
         mPref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
         medit = mPref.edit();
+        mLocationInfo=new LocationInfo();
         fn_permission();
         if (boolean_permission) {
 
@@ -103,8 +136,77 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        buildGoogleApiClient();
+        init();
+
+    }
+    private void moveCamera(LatLng latLng, float zoom, String title){
+        Log.d(TAG, "moveCamera: moving the camera to: lat: " + latLng.latitude + ", lng: " + latLng.longitude );
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
+
+        if(!title.equals("My Location")){
+            MarkerOptions options = new MarkerOptions()
+                    .position(latLng)
+                    .title(title);
+            mMap.addMarker(options);
+        }
+
+    }
+    private void init(){
+        Log.d(TAG, "init: initializing");
+        mAutocompleteAdapter = new PlaceAutocompleteAdapter(this, mGoogleApiClient,
+                LAT_LNG_BOUNDS, null);
+        remainderAddress.setAdapter(mAutocompleteAdapter);
+
+        remainderAddress.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) {
+                if(actionId == EditorInfo.IME_ACTION_SEARCH
+                        || actionId == EditorInfo.IME_ACTION_DONE
+                        || keyEvent.getAction() == KeyEvent.ACTION_DOWN
+                        || keyEvent.getAction() == KeyEvent.KEYCODE_ENTER){
+                }
+
+                return false;
+            }
+
+        });
     }
 
+    protected synchronized void buildGoogleApiClient(){
+        mGoogleApiClient=new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addApi(Places.GEO_DATA_API)
+                .addApi(Places.PLACE_DETECTION_API)
+                .build();
+        remainderAddress.setOnItemClickListener(mAutoCompleteListener);
+        mAutocompleteAdapter = new PlaceAutocompleteAdapter(this,mGoogleApiClient,null,null);
+        mGoogleApiClient.connect();
+
+    }
+    private AdapterView.OnItemClickListener mAutoCompleteListener=new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            final AutocompletePrediction item=mAutocompleteAdapter.getItem(i);
+            final String placeId=item.getPlaceId();
+            PendingResult<PlaceBuffer> placeResult=Places.GeoDataApi.getPlaceById(mGoogleApiClient,placeId);
+            placeResult.setResultCallback(mUpdatePlaceDetailsCallback);
+        }
+    };
+    private ResultCallback<PlaceBuffer> mUpdatePlaceDetailsCallback=new ResultCallback<PlaceBuffer>() {
+        @Override
+        public void onResult(@NonNull PlaceBuffer places) {
+            if(!places.getStatus().isSuccess()){
+                places.release();
+                return;
+            }
+            final Place place=places.get(0);
+            mLocationInfo.setLatLng(new LatLng(place.getViewport().getCenter().latitude,place.getViewport().getCenter().longitude));
+            if(mLocationInfo.getLatLng()!=null)
+                moveCamera(mLocationInfo.getLatLng(),15,"Dhaka");
+            places.release();
+        }
+    };
     private BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
@@ -123,11 +225,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             } catch (IOException e1) {
                 e1.printStackTrace();
             }
-
-
             currentAddress.setText(latitude + " " + longitude);
-
-
         }
     };
 
--- app/src/main/java/com/placeremainder/place/placealertassistent/models/LocationInfo.java
+++ app/src/main/java/com/placeremainder/place/placealertassistent/models/LocationInfo.java
@@ -1,8 +1,34 @@
 package com.placeremainder.place.placealertassistent.models;
 
-/**
- * Created by Habib on 19/10/18.
- */
+
+import com.google.android.gms.maps.model.LatLng;
 
 public class LocationInfo {
+    public LatLng getLatLng() {
+        return latLng;
+    }
+
+    public void setLatLng(LatLng latLng) {
+        this.latLng = latLng;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public double getLng() {
+        return lng;
+    }
+
+    public void setLng(double lng) {
+        this.lng = lng;
+    }
+
+    private LatLng latLng;
+    private double lat;
+    private double lng;
 }
